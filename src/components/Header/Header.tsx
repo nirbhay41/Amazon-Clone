1245005a28f2f87b29066dbf8402d9df5dcd867f
Nirbhay <nirbhay411@gmail.com>
2021-05-28 17:16:38 +0530

    Added Lower Nav and Sidebar Component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,101 @@
 import Image from "next/image";
 import styles from './Header.module.scss';
-import { ShoppingCartIcon } from '@heroicons/react/outline';
+import { ShoppingCartIcon, MenuIcon } from '@heroicons/react/outline';
 import SearchBar from './SearchBar/SearchBar';
 import UserLogin from './UserLogin/UserLogin';
+import SideNav from "./SideNav/SideNav";
+import { useEffect, useRef, useState } from "react";
+
+async function getCategories(): Promise<Array<string>> {
+    const res = await fetch('https://fakestoreapi.com/products/categories');
+    const categories = await res.json();
+    return categories;
+}
 
 export default function Header() {
+    const [categories, setCategories] = useState<string[]>([]);
+    const sideNavRef = useRef<HTMLDivElement>(null);
+    const overlayRef = useRef<HTMLDivElement>(null);
+    const closeBtnRef = useRef<HTMLDivElement>(null);
+    const [menu, setMenu] = useState(false);
+
+    useEffect(() => {
+        getCategories().then(res => setCategories(res));
+    }, []);
+
+    const openSideNav = () => {
+        setMenu(true);
+        sideNavRef.current.style.width = "370px";
+        overlayRef.current.style.display = "block";
+        closeBtnRef.current.style.display = "block";
+    }
+
     return (
-        <header className={styles.header}>
-            <div className={styles.top_nav}>
-                <div className={styles.nav_logo}>
-                    <Image
-                        className={styles.logo}
-                        src="/amazon_logo_primary.png"
-                        width={110}
-                        height={110}
-                        objectFit="contain"
-                    />
-                    <span>.in</span>
-                </div>
-
-                <div className={styles.searchBar}>
-                    <SearchBar />
-                </div>
-
-                <div className={styles.userlogin}>
-                    <UserLogin />
-                </div>
-
-                <div className={styles.orders}>
-                    <div>Returns</div>
-                    <p>&Orders</p>
-                </div>
-
-                <div className={styles.cart}>
-                    <div className={styles.cart_no} style={{position: "relative"}}>
-                        <span className={styles.order_no}>0</span>
-                        <ShoppingCartIcon className={styles.carticon} />
+        <>
+            <SideNav categories={categories}
+                sideNavRef={sideNavRef}
+                overlayRef={overlayRef}
+                closeBtnRef={closeBtnRef}
+                menu={menu}
+                setMenu={setMenu}
+            />
+            <header className={styles.header}>
+                <div className={styles.top_nav}>
+                    <div className={styles.nav_logo}>
+                        <Image
+                            className={styles.logo}
+                            src="/amazon_logo_primary.png"
+                            width={110}
+                            height={110}
+                            objectFit="contain"
+                        />
+                        <span>.in</span>
                     </div>
-                    <span className={styles.cart_text}>Cart</span>
-                </div>
-            </div>
 
-            <div className={styles.lower_nav}>
-            </div>
-        </header>
+                    <div className={styles.searchBar}>
+                        <SearchBar categories={categories} />
+                    </div>
+
+                    <div className={styles.userlogin}>
+                        <UserLogin />
+                    </div>
+
+                    <div className={styles.orders}>
+                        <div>Returns</div>
+                        <p>&Orders</p>
+                    </div>
+
+                    <div className={styles.cart}>
+                        <div className={styles.cart_no} style={{ position: "relative" }}>
+                            <span className={styles.order_no}>0</span>
+                            <ShoppingCartIcon className={styles.carticon} />
+                        </div>
+                        <span className={styles.cart_text}>Cart</span>
+                    </div>
+                </div>
+
+                <div className={styles.lower_nav}>
+                    <div className={styles.all_menu} onClick={openSideNav}>
+                        <MenuIcon className={styles.menu_icon} />
+                        <span>All</span>
+                    </div>
+
+                    <p className={styles.text}>Prime Video</p>
+                    <p className={styles.text}>Amazon Business</p>
+                    <p className={styles.text}>Today's Deals</p>
+                    <div className={styles.extra}>
+                        <p className={styles.text}>Electronics</p>
+                        <p className={styles.text}>Food & Grocery</p>
+                        <p className={styles.text}>Prime</p>
+                        <p className={styles.text}>Buy Again</p>
+                        <p className={styles.text}>Shopper Toolkit</p>
+                        <div className={styles.banner}>
+                            <Image src="/amazon_music_banner.webp" height={40}
+                                width={390} />
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
     );
 }
---
+++ src/components/Header/SideNav/SideNav.tsx
@@ -0,0 +1,80 @@
+import { ReactElement, RefObject, useEffect } from 'react';
+import styles from './SideNav.module.scss';
+import { UserCircleIcon, ChevronRightIcon } from '@heroicons/react/solid';
+
+type SideNavProps = {
+    sideNavRef: RefObject<HTMLDivElement>;
+    overlayRef: RefObject<HTMLDivElement>;
+    closeBtnRef: RefObject<HTMLDivElement>;
+    categories: string[];
+    menu: boolean;
+    setMenu: Function;
+}
+
+export default function SideNav({ categories, sideNavRef, overlayRef, closeBtnRef,menu,setMenu }: SideNavProps) {
+    const closeSideNav = () => {
+        closeBtnRef.current.style.display = "none";
+        sideNavRef.current.style.width = "0";
+        overlayRef.current.style.display = "none";
+    }
+
+    return (
+        <>
+            <div className={styles.sidenav} ref={sideNavRef}>
+                <div className={styles.sidenav_bar}>
+                    <div className={styles.user}>
+                        <UserCircleIcon className={styles.userIcon} />
+                        <span className={styles.text}>Hello, Nirbhay</span>
+                    </div>
+
+                    <div className={styles.menu_item}>
+                        <div className={styles.title}>Shop By Department</div>
+                        <div className={styles.options}>
+                            <ul>
+                                {categories.map((e, idx) => (
+                                    <li key={idx}><MenuOption name={e} /></li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={styles.menu_item}>
+                        <div className={styles.title}>Digital Content And Devices</div>
+                        <div className={styles.options}>
+                            <ul>
+                                <li key="Echo & Alexa"><MenuOption name="Echo & Alexa"/></li>
+                                <li key="Fire TV"><MenuOption name="Fire TV"/></li>
+                                <li key="Kindle E-Readers & eBooks"><MenuOption name="Kindle E-Readers & eBooks"/></li>
+                                <li key="Amazon Prime Video"><MenuOption name="Amazon Prime Video"/></li>
+                                <li key="Amazon Prime Music"><MenuOption name="Amazon Prime Music"/></li>
+                                <li key="Audible AudioBooks"><MenuOption name="Audible AudioBooks"/></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={styles.menu_item}>
+                        <div className={styles.title}>Help & Settings</div>
+                        <div className={styles.options}>
+                            <ul>
+                                <li key={1}><MenuOption name="Your Account"/></li>
+                                <li key={2}><MenuOption name="Customer Service"/></li>
+                                <li key={3}><MenuOption name="Sign Out"/></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.closebtn} onClick={closeSideNav} ref={closeBtnRef}>&times;</div>
+            </div>
+            <div className={styles.overlay} onClick={closeSideNav} ref={overlayRef}></div>
+        </>
+    )
+}
+
+function MenuOption({ name }: { name: string }): ReactElement {
+    return (
+        <div className={styles.menu_option}>
+            <div className={styles.menu_name}>{name}</div>
+            <ChevronRightIcon className={styles.chevron_right} />
+        </div>
+    );
+}
