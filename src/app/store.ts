9cac296cf26a8fa7e39ddf93c9e8d526d7139bea
Nirbhay <nirbhay411@gmail.com>
2021-06-16 10:29:20 +0530

    Persisted the redux state after page reload or closing the page with redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import basketReducer from './slices/basketSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, basketReducer);
+
+const customizedMiddleware = getDefaultMiddleware({
+    serializableCheck: false
+})
 
 export const store = configureStore({
-    reducer:{
-        basket:basketReducer,
-    }
-})
+    reducer: {
+        basket: persistedReducer
+    },
+    middleware: customizedMiddleware,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import { store } from '../app/store';
+import { persistor, store } from '../app/store';
 import { Provider as AuthProvider } from 'next-auth/client';
 import '../styles/globals.css'
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider session={pageProps.session}>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </AuthProvider>
   );
