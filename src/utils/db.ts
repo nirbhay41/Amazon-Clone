e6fad80c2920109867fc9aa77445de16d5a6b206
Nirbhay <nirbhay411@gmail.com>
2021-06-21 14:13:48 +0530

    Changed MONGODBURI and DB env variable

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,22 +1,22 @@
 import { MongoClient } from "mongodb";
 
-const { MONGODB_URI_LOCAL, MONGODB_DB_LOCAL } = process.env;
+const { MONGODB_URI, MONGODB_DB } = process.env;
 
-if (!MONGODB_URI_LOCAL) {
+if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
-if (!MONGODB_DB_LOCAL) {
+if (!MONGODB_DB) {
     throw new Error('Please define the MONGODB_DB environment variable inside .env.local');
 }
 
-const client = new MongoClient(MONGODB_URI_LOCAL, {
+const client = new MongoClient(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 export async function connectToDatabase() {
     if (!client.isConnected()) await client.connect();
-    const db = client.db(MONGODB_DB_LOCAL);
+    const db = client.db(MONGODB_DB);
     return { client, db };
 }
